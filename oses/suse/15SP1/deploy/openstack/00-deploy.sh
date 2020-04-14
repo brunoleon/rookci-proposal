f252753ba2b0b815949be908dca9a9b1e8870020
root <root@opensuse2.myguest.virtualbox.org>
2020-04-14 18:21:16 +0200

    Working set

--- oses/suse/15SP1/deploy/openstack/00-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-terraform apply -auto-approve
-skuba cluster init --control-plane $TR_IP_LB cluster
-cd cluster
-skuba node bootstrap --user sles --sudo --target $TR_IP_MASTER my-master
-skuba node join --role worker --user sles --sudo --target $TR_WORKER1 worker1
-skuba node join --role worker --user sles --sudo --target $TR_WORKER2 worker2
--- test.py
+++ test.py
@@ -3,13 +3,18 @@
 import lib.log
 import os
 import argparse
-from ruamel import yaml
+import subprocess
+import ruamel.yaml
+import random
+import string
 logger = lib.log.setup_logging(__name__)
+
 
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--os",
                         type=str,
+                        action='append',
                         required=True,
                         help="OS to test")
     return vars(parser.parse_args())
@@ -21,7 +26,7 @@ def main():
             args = parse()
             args['basepath'] = os.getcwd()
             work(args)
-        except Exception as e:
+        except:
             logger.critical(
                 'Program failed unexpectedy. Check debuglog for details.')
             raise
@@ -34,10 +39,15 @@ def main():
 def work(args):
     oses = []
     for distrib in os.listdir('oses'):
+        if distrib not in args['os']:
+            if 'all' not in args['os']:
+                logger.info('Skipping distribution {}'.format(distrib))
+                continue
         for version in os.listdir(os.path.join('oses', distrib)):
             oses.append(OS(args, distrib, version))
     for element in oses:
-        element.deploy_infra()
+        print('FLAG XXXXXXXXXXXXXXx')
+        element.run()
 
 
 class OS:
@@ -45,22 +55,101 @@ class OS:
         self.args = args
         self.distrib = distrib
         self.version = version
+        self.root = os.path.join(args['basepath'], 'oses', distrib, version)
+        logger.info('Loaded OS from {}'.format(self.root))
 
-    def deploy_infra(self):
-        root = os.path.join(self.args['basepath'], 'oses', self.distrib,
-                            self.version, 'deploy')
-        for element in os.listdir(root):
-            fp = os.path.join(root, element)
-            if not os.path.isfile(fp) or \
-                    not os.path.splitext(fp)[-1] in ['.sh', '.py']:
-                continue
-            os.chdir(root)
-            os.system(fp)
+    def load_cases(self):
+        self.cases = []
+        try:
+            specs = read_yaml(os.path.join(self.root, 'specs.yaml'))
+            for _id in specs['cases'].keys():
+                self.cases.append(Case(self.root, _id, specs['cases'][_id]))
+        except:
+            logger.error('Unable to load cases for {}/{}'.format(
+                self.distrib, self.version))
 
-    def deploy_k8s():
+    def run(self):
+        self.load_cases()
+        for case in self.cases:
+            case.run()
+
+
+class Case:
+    def __init__(self, root, _id, case):
+        self.root = root
+        self._id = _id
+        self.case = case
+        self.name = gen_random_string(prefix='ci-%d-' % self._id, length=10)
+        logger.info('Loaded case {}'.format(_id))
+
+    def run(self):
+        try:
+            logger.info('Running case with name {}'.format(self.name))
+            self.deploy_infra(
+                self.name,
+                self.case['infra']['script'],
+                self.case['infra']['master'],
+                self.case['infra']['worker'])
+            self.deploy_k8s(self.case['infra']['script'])
+            self.deploy_rook()
+        finally:
+            self.undeploy_infra(
+                self.name,
+                self.case['infra']['script'],
+            )
+
+
+    def deploy_infra(self, name, script, master, worker):
+        logger.info("Deploying infra")
+        root = os.path.join(self.root, 'deploy')
+        try:
+            subprocess.run(['./{}'.format(script),
+                            '--action', 'deploy',
+                            '--name', name,
+                            '--master', str(master),
+                            '--worker', str(worker)],
+                           cwd=root)
+        except:
+            raise
+
+    def undeploy_infra(self, name, script):
+        logger.info("Undeploying infra")
+        root = os.path.join(self.root, 'deploy')
+        subprocess.run(['./{}'.format(script),
+                        '--action', 'undeploy',
+                        '--name', name],
+                       cwd=root)
+
+    def deploy_k8s(self, script):
+        logger.info("Deploying Kubernetes")
+        root = os.path.join(self.root, 'deploy')
+        try:
+            subprocess.run(['./{}'.format(script), cwd=root)
+        except:
+            raise
+
+    def deploy_rook(self):
         pass
 
-    def deploy_rook():
-        pass
+
+def write_yaml(data, outfile):
+    with open(outfile, 'w') as fp:
+        fp.write(ruamel.yaml.dump(
+            data,
+            default_flow_style=False,
+            explicit_start=True))
+
+
+def read_yaml(template):
+    with open(template, 'r') as fp:
+        model = ruamel.yaml.load(fp, Loader=ruamel.yaml.Loader)
+    return model
+
+
+def gen_random_string(prefix='', length=10):
+    return '{}-{}'.format(
+        prefix,
+        ''.join(random.choice(string.ascii_letters) for x in range(length)))
+
 
 main()
